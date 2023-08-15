4e5a56f82cd9de0868db75bef194722d20c4aad2
Arjun Sharma <arjunsharma6622@gmail.com>
2023-08-16 03:29:12 +0530

    added kruskals

--- 4a_Kruskals.c
+++ 4a_Kruskals.c
@@ -4,28 +4,65 @@
 #define MAX_VERTICES 100
 #define INF 999999
 
-
-// structure to represent an edge
-struct Edge{
+struct Edge {
     int src, dest, weight;
 };
 
 int parent[MAX_VERTICES];
 
-//find set of an element
-int find(int i){
+int find(int i) {
     while (parent[i] != i)
-    {
         i = parent[i];
-    }
-    return i; 
+    return i;
 }
 
-//union of two sets
-void unionSets(int i, int j){
+void unionSets(int i, int j) {
     int a = find(i);
     int b = find(j);
-
     parent[a] = b;
 }
 
+int compare(const void* a, const void* b) {
+    return ((struct Edge*)a)->weight - ((struct Edge*)b)->weight;
+}
+
+void kruskal(struct Edge edges[], int numEdges, int numVertices) {
+    qsort(edges, numEdges, sizeof(struct Edge), compare);
+
+    struct Edge result[numVertices];
+    int e = 0, i = 0;
+
+    for (int v = 0; v < numVertices; v++)
+        parent[v] = v;
+
+    while (e < numVertices - 1) {
+        struct Edge nextEdge = edges[i++];
+        int x = find(nextEdge.src);
+        int y = find(nextEdge.dest);
+        if (x != y) {
+            result[e++] = nextEdge;
+            unionSets(x, y);
+        }
+    }
+
+    printf("Minimum Spanning Tree (Kruskal's algorithm):\n");
+    for (i = 0; i < e; i++)
+        printf("%d - %d\n", result[i].src, result[i].dest);
+}
+
+int main() {
+    int numVertices, numEdges;
+    printf("Enter the number of vertices and edges: ");
+    scanf("%d %d", &numVertices, &numEdges);
+
+    struct Edge edges[numEdges];
+    
+    printf("Enter edges and weights (src dest weight):\n");
+    for (int i = 0; i < numEdges; i++) {
+        scanf("%d %d %d", &edges[i].src, &edges[i].dest, &edges[i].weight);
+    }
+
+    kruskal(edges, numEdges, numVertices);
+
+    return 0;
+}
